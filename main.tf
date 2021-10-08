464a79fa680f5c0f4050cc24d08c46e7046d509c
implodingduck <zsek320@gmail.com>
2021-10-08 08:53:50 -0500

    adding an ACR

--- main.tf
+++ main.tf
@@ -103,3 +103,10 @@ resource "azurerm_kubernetes_cluster" "example" {
   tags = local.tags
 }
 
+resource "azurerm_container_registry" "test" {
+  name                = "acr${local.cluster_name}"
+  resource_group_name = azurerm_resource_group.aks.name
+  location            = azurerm_resource_group.aks.location
+  sku                 = "Standard"
+
+}
